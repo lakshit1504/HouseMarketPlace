22cdfdac81aa5405751617c704e15748dca62a6b
lakshit1504 <121532634+lakshit1504@users.noreply.github.com>
2023-09-04 00:15:35 +0530

    firebase auth change

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import {getFirestore} from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB-jrhynei-wnE9QSdP_g0k582R7JDf344",
-  authDomain: "house-marketplace-app-632ce.firebaseapp.com",
-  projectId: "house-marketplace-app-632ce",
-  storageBucket: "house-marketplace-app-632ce.appspot.com",
-  messagingSenderId: "757534614044",
-  appId: "1:757534614044:web:a3d9cad1602d76056775d9"
+  apiKey: "AIzaSyCYSWilmmxgvyLYFgXSCttBOLGAGRmfKVE",
+  authDomain: "housemarketplace-34b0e.firebaseapp.com",
+  projectId: "housemarketplace-34b0e",
+  storageBucket: "housemarketplace-34b0e.appspot.com",
+  messagingSenderId: "236283975749",
+  appId: "1:236283975749:web:1e66093f38b6249652ab2c"
 };
 
 // Initialize Firebase
